fc4088970b810c7b43a2663ce7dd3bc9719c2269
Nyong Nsikak <nyongconnect@gmail.com>
2022-11-06 19:01:46 +0100

    make status bar transparent

--- app/src/main/java/com/example/pallytest/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pallytest/presentation/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.pallytest.presentation
 
+import android.annotation.TargetApi
+import android.app.Activity
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.example.pallytest.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,6 +15,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setStatusBarGradiant(this)
         setContentView(R.layout.activity_main)
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    fun setStatusBarGradiant(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = activity.window
+            val background = ContextCompat.getDrawable(activity, R.drawable.background_gradient)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+
+            window.statusBarColor = ContextCompat.getColor(activity,android.R.color.transparent)
+            window.navigationBarColor = ContextCompat.getColor(activity,android.R.color.transparent)
+            window.setBackgroundDrawable(background)
+        }
     }
 }
